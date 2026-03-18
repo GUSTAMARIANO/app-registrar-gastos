f5dba3964c0349f073051ef37c9a03ab750f8640
GUSTAMARIANO <luizguga235711@gmail.com>
2026-03-18 06:36:29 -0300

    ativação da camera

---
+++ app/src/main/java/com/example/camerax/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.camerax;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import android.graphics.Matrix;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnpicture;
+    ImageView imageView;
+    TextView texto;
+
+    ActivityResultLauncher<Intent> cameraLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnpicture = findViewById(R.id.btncamera_id);
+        imageView = findViewById(R.id.imageview1);
+        texto = findViewById(R.id.text);
+
+
+        //Variavéis responsaveis por armazena a foto
+        File photoFile = new File(getExternalFilesDir(null), "photo.jpg");
+        Uri photoURI = FileProvider.getUriForFile(
+                this,
+                getPackageName() + ".provider",
+                photoFile
+        );
+
+        //Ativação do método após o evento de click
+        btnpicture.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            cameraLauncher.launch(cameraIntent);
+        });
+
+
+
+        // Metodo responsavel por exibir a foto
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+
+                    if (result.getResultCode() == RESULT_OK) {
+
+                        Intent data = result.getData();
+                            texto.setText("teste");
+                            Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                            imageView.setImageBitmap(bitmap);
+
+                    }
+                }
+        );
+    }
+}
